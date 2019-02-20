efd66862cc25057893c46f0ccf9e8e22d03ea094
linyongzhi <yongzhi.lin@uama.com.cn>
2019-02-20 15:26:17 +0800

    完成第一个三角形的绘制

--- MetalByExample/MetalView.m
+++ MetalByExample/MetalView.m
@@ -15,6 +15,13 @@
 
 @property (nonatomic, strong) id <MTLDevice> device;
 
+@property (nonatomic, strong) id <MTLBuffer> colorBuffer;
+@property (nonatomic, strong) id <MTLBuffer> positionBuffer;
+
+@property (nonatomic, strong) id <MTLRenderPipelineState> pipeline;
+
+@property (nonatomic, strong) CADisplayLink *displayLink;
+
 @end
 
 @implementation MetalView
@@ -26,6 +33,7 @@
     // Drawing code
 }
 */
+
 + (id)layerClass {
     return [CAMetalLayer class];
 }
@@ -33,15 +41,64 @@
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        _device = MTLCreateSystemDefaultDevice();
-        _metalLayer = (CAMetalLayer *)[self layer];
-        _metalLayer.device = _device;
-        _metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+        [self buildDevice];
+        [self buildVertexBuffers];
+        [self buildPipeline];
     }
     return self;
 }
 
-- (void)didMoveToWindow {
+- (void)buildDevice {
+    _device = MTLCreateSystemDefaultDevice();
+    _metalLayer = (CAMetalLayer *)[self layer];
+    _metalLayer.device = _device;
+    _metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+}
+
+- (void)buildVertexBuffers {
+    static const float positions[] = {
+         0.0,  0.0, 0, 1,
+        -0.5, -0.5, 0, 1,
+         0.5, -0.5, 0, 1,
+    };
+    
+    static const float colors[] = {
+        1, 0, 0, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+    };
+    
+    self.positionBuffer = [self.device newBufferWithBytes:positions length:sizeof(positions) options:MTLResourceOptionCPUCacheModeDefault];
+    self.colorBuffer = [self.device newBufferWithBytes:colors length:sizeof(colors) options:MTLResourceOptionCPUCacheModeDefault];
+}
+
+- (void)buildPipeline {
+    id<MTLLibrary> library = [self.device newDefaultLibrary];
+    
+    id<MTLFunction> vertexFunc = [library newFunctionWithName:@"vertex_main"];
+    id<MTLFunction> fragmentFunc = [library newFunctionWithName:@"fragment_main"];
+    
+    MTLRenderPipelineDescriptor *pipelineDescriptor = [MTLRenderPipelineDescriptor new];
+    pipelineDescriptor.vertexFunction = vertexFunc;
+    pipelineDescriptor.fragmentFunction = fragmentFunc;
+    pipelineDescriptor.colorAttachments[0].pixelFormat = self.metalLayer.pixelFormat;
+    
+    self.pipeline = [self.device newRenderPipelineStateWithDescriptor:pipelineDescriptor error:nil];
+}
+
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+    
+    if (self.superview) {
+        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkDidFire:)];
+        [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    } else {
+        [self.displayLink invalidate];
+        self.displayLink = nil;
+    }
+}
+
+- (void)displayLinkDidFire:(CADisplayLink *)displayLink {
     [self redraw];
 }
 
@@ -53,11 +110,17 @@
     passDescriptor.colorAttachments[0].texture = texture;
     passDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
     passDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
-    passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(1.0, 0.0, 0.0, 1.0);
+    passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1.0);
     
     id<MTLCommandQueue> commandQueue = [self.device newCommandQueue];
     id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
-    id<MTLCommandEncoder> commandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
+    id<MTLRenderCommandEncoder> commandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
+    
+    [commandEncoder setRenderPipelineState:self.pipeline];
+    [commandEncoder setVertexBuffer:self.positionBuffer offset:0 atIndex:0];
+    [commandEncoder setVertexBuffer:self.colorBuffer offset:0 atIndex:1];
+    [commandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3 instanceCount:1];
+    
     [commandEncoder endEncoding];
     
     [commandBuffer presentDrawable:drawable];
---
+++ MetalByExample/Shaders.metal
@@ -0,0 +1,28 @@
+//
+//  Shaders.metal
+//  MetalByExample
+//
+//  Created by linyongzhi on 2019/2/20.
+//  Copyright © 2019 BabyTiger. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct ColoredVertex {
+    float4 position [[position]];
+    float4 color;
+};
+
+vertex ColoredVertex vertex_main(constant float4 *position [[buffer(0)]],
+                                 constant float4 *color [[buffer(1)]],
+                                 uint vid [[vertex_id]]) {
+    ColoredVertex vert;
+    vert.position = position[vid];
+    vert.color = color[vid];
+    return vert;
+}
+
+fragment float4 fragment_main(ColoredVertex vert [[stage_in]]) {
+    return vert.color;
+}
